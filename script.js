a39d0ace33f6f12765940832999d1c199a8144c8
Andrew Huynh <andhuynh24@gmail.com>
2022-08-16 04:02:50 -0700

    Added basic functionality to game

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 // Add HTML content
-const container = document.getElementById('container');
+const container = document.querySelector("#container");
 
-for(let i = 0; i < 256; i++){
+for (let i = 0; i < 256; i++) {
     const box = document.createElement('div');
     box.classList.add('box');
+    box.addEventListener('mouseover', function(event){
+        event.target.style.backgroundColor = 'black';
+    });
     container.appendChild(box);
 }
+
+
